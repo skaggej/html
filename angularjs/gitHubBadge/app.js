160b5915e93983c8ae4ae9be3302548a433edc82
Eric Skaggs <eskaggs@outlook.com>
2016-08-03 18:47:20 -0700

    Added gitHubBadge
    A little GitHub badge action with AngularJS

---
+++ angularjs/gitHubBadge/app.js
@@ -0,0 +1,8 @@
+﻿//define controller
+function githubCtl($scope, $http) {
+    var url = 'https://api.github.com/users/skaggej';
+    $http.get(url).then(function (resp) { $scope.data = resp.data; });
+}
+
+//register module and controller
+angular.module('githubApp', []).controller('githubCtl', githubCtl);
